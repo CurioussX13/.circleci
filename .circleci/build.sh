d701e2fdb11596931413078e7a71d65afb841f9e
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2019-12-26 16:26:35 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,29 +1,20 @@
 #!/usr/bin/env bash
-apt-get update -y
-sudo apt-get install -y ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python m4 gcc libtool zlib1g-dev dash
+sudo apt-get update -y
+sudo apt-get install -y ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python m4 gcc libtool zlib1g-dev dash 
 cd /
-git clone https://github.com/NusantaraDevs/clang --depth=1 -b dev/10.0 clang
-git clone https://github.com/Nicklas373/kernel_xiaomi_msm8953-3.18-2 -b dev/kasumi-10 mido --depth 1
+git clone  https://github.com/dracarys18/NotKernel.git -b pie mido --depth 1
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r20 gcc32 --depth 1 
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r20 gcc --depth 1
 export ARCH=arm64
 export SUBARCH=arm64
-export LD_LIBRARY_PATH=/clang/lib
-export CLANG_TRIPLE=/clang/bin/aarch64-linux-gnu-
-export CLANG_TRIPLE_ARM32=/clang/bin/arm-linux-gnueabi-
-export CROSS_COMPILE=/clang/bin/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=/clang/bin/arm-linux-gnueabi-
-export CC=/clang/bin/clang
-	cd mido
-        make  O=out ARCH=arm64 mido_defconfig
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=/clang/bin/clang \
-                    CLANG_TRIPLE=/clang/bin/aarch64-linux-gnu- \
-CLANG_TRIPLE_ARM32=/clang/bin/arm-linux-gnueabi- \
- CROSS_COMPILE=/clang/bin/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=/clang/bin/arm-linux-gnueabi-
+export CROSS_COMPILE=/gcc/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/gcc32/bin/arm-linux-androideabi-
+cd mido
+ make  O=out ARCH=arm64 mido_defconfig
+make -j16 O=out 
 
 
-
+              
         
 			
 	
