187d53a855bd4d7253db28762759cd330f3fab81
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2019-12-26 16:30:38 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo apt-get update -y
-sudo apt-get install -y ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python m4 gcc libtool zlib1g-dev dash 
+apt-get update -y
+ apt-get install -y ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python m4 gcc libtool zlib1g-dev dash 
 cd /
 git clone  https://github.com/dracarys18/NotKernel.git -b pie mido --depth 1
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r20 gcc32 --depth 1 
