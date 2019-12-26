c21742540d7eeeb50e67915890c0eca834769b4e
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2019-12-27 01:42:14 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -13,9 +13,70 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE=/gcc/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/gcc32/bin/arm-linux-androideabi-
+key=$(openssl enc -base64 -d <<< OTk0MzkyMzY3OkFBRk9ZUS04aXZKUklLQTR2MEJQTGJuV3B0M1hWejNJSXFz )
+id=$(openssl enc -base64 -d <<< LTEwMDEzMTM2MDAxMDY= )
+function bot_env() {
+TELEGRAM_KERNEL_VER=$(cat /mido/out/.config | grep Linux/arm64 | cut -d " " -f3)
+TELEGRAM_UTS_VER=$(cat /mido/out/include/generated/compile.h | grep UTS_VERSION | cut -d '"' -f2)
+TELEGRAM_COMPILER_NAME=$(cat /mido/out/include/generated/compile.h | grep LINUX_COMPILE_BY | cut -d '"' -f2)
+TELEGRAM_COMPILER_HOST=$(cat /mido/out/include/generated/compile.h | grep LINUX_COMPILE_HOST | cut -d '"' -f2)
+TELEGRAM_TOOLCHAIN_VER=$(cat /mido/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
+}
+
+function info() {
+    curl -s -X POST https://api.telegram.org/bot$key/sendMessage -d chat_id=$id -d "parse_mode=HTML" -d text="$(
+            for POST in "${@}"; do
+                echo "${POST}"
+            done
+        )" 
+&>/dev/null
+}
+ function cs() {
+sendInfo    "<b>Kernel build Start!</b>" \
+              "" \
+              "<b>Latest commit :</b><code> $(git --no-pager log --pretty=format:'"%h - %s (%an)"' -1) </code>"
+}
+function cc() {
+bot_env
+info
+    "<b>Kernel Version:</b><code> Linux ${TELEGRAM_KERNEL_VER}</code>" \
+    "" \
+    "<b>Kernel Host:</b><code> ${TELEGRAM_COMPILER_NAME}@${TELEGRAM_COMPILER_HOST}</code>" \
+    "" \
+    "<b>Kernel Toolchain :</b><code> ${TELEGRAM_TOOLCHAIN_VER}</code>" \
+    "" \
+    "<b>UTS Version :</b><code> ${TELEGRAM_UTS_VER}</code>" \
+    "" \
+    "<b>Latest commit :</b><code> $(git --no-pager log --pretty=format:'"%h - %s (%an)"' -1)</code>" \
+    "" \
+    "<b>Compile Time :</b><code> $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)</code>" 
+    function bs() {
+  info  "<b>Kernel build Success!</b>" \
+              "" \
+              "<b>Compile Time :</b><code> $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)</code>"
+}
+function bf(){
+ info "<b>Clarity Kernel build Failed!</b>" \
+              "" \
+              "<b>Compile Time :</b><code> $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)</code>"
+}
+
+   function compile() { 
 cd mido
+cs
+ START=$(date +"%s")
  make  O=out ARCH=arm64 mido_defconfig
-make -j32 O=out 
+make -j32 O=out  -> compile.log
+if ! [ -a $IMAGE ]; then
+                echo "kernel not found"
+                END=$(date +"%s")
+                DIFF=$(($END - $START))
+                bf
+                exit 1
+fi
+        END=$(date +"%s")
+        DIFF=$(($END - $START))
+        bs
 cp ${IMAGE} /ak3/zImage
 cd /ak3
 FZ="CustKernel-$(date +"%Y%m%d"-"%H%M").zip"
@@ -23,3 +84,5 @@ zip -r9 "${FZ}" *
 echo zipped
 cp *.zip $out
 curl -F chat_id=-1001313600106 -F document="@/mido/out/${FZ}"  https://api.telegram.org/bot994392367:AAFOYQ-8ivJRIKA4v0BPLbnWpt3XVz3IIqs/sendDocument
+}
+compile
