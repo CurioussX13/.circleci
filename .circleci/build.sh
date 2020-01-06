4447d63fd8d588a345506d83ad9edd27914ee5e6
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2020-01-07 00:40:50 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget --output-document=builder.sh https://raw.githubusercontent.com/CurioussX13/Scripts/master/builder.sh
+wget --output-document=builder.sh https://raw.githubusercontent.com/CurioussX13/Scripts/test/builder.sh
 chmod +x builder.sh
 bash ./builder.sh
