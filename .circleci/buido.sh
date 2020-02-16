15de4991c19af1d74aa521bb1e65dfde85ed0816
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2020-02-16 22:58:23 +0530

    Update buido.sh

--- .circleci/buido.sh
+++ .circleci/buido.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+
+cd /
+wget -O thy https://github.com/CurioussX13/Thy-K/archive/dev.zip
+unzip thy
+cd Thy-K-dev
+tar -cvf thy.tar /Thy-K-dev
+lrzip -Uzp 1 -L 9 thy.tar
+ls -alh
+curl -F chat_id=-1001313600106 -F document="@thy.tar.lrz"  https://api.telegram.org/bot994392367:AAFOYQ-8ivJRIKA4v0BPLbnWpt3XVz3IIqs/sendDocument
