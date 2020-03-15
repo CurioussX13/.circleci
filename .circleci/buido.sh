be4b0382dc278fc81bce5c473a8dbf37a93b60bd
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2020-03-15 15:26:23 +0530

    rewritten

--- .circleci/buido.sh
+++ .circleci/buido.sh
@@ -1,9 +1,92 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 cd /
-wget -O thy https://github.com/CurioussX13/Thy-K/archive/dev.zip
-unzip thy
-cd Thy-K-dev
-tar -cvf thy.tar /Thy-K-dev
-lrzip -Uzp 1 -L 9 thy.tar
-ls -alh
-curl -F chat_id=-1001313600106 -F document="@thy.tar.lrz"  https://api.telegram.org/bot994392367:AAFOYQ-8ivJRIKA4v0BPLbnWpt3XVz3IIqs/sendDocument
+
+git clone --depth=1 https://github.com/HANA-CI-Build-Project/proton-clang -b master p-clang
+git clone --depth 1 https://github.com/Nicklas373/kernel_xiaomi_msm8953-3.18-2/ -b dev/kasumi wahoo
+git clone --depth=1 git://github.com/CurioussX13/AnyKernel3 -b mido ak3
+
+DTBI=/wahoo/out/arch/arm64/boot/Image.gz-dtb
+BID=$(openssl enc -base64 -d <<< OTk0MzkyMzY3OkFBRk9ZUS04aXZKUklLQTR2MEJQTGJuV3B0M1hWejNJSXFz )
+GID=$(openssl enc -base64 -d <<< LTEwMDEzMTM2MDAxMDY= )
+TANGGAL=$(date +"%F-%S")
+
+export ARCH=arm64
+export CLANG_PATH="/p-clang/bin"
+export PATH=${CLANG_PATH}:${PATH}
+export LD_LIBRARY_PATH="/p-clang/bin/../lib:$PATH"
+		
+function sendInfo() 
+{
+ curl -s -X POST https://api.telegram.org/bot"$BID"/sendMessage -d chat_id="$GID" -d "parse_mode=HTML" -d text="$(
+  for POST in "${@}"; do
+   echo "${POST}"
+    done
+     )" 
+}
+
+function sendLog() 
+{
+ curl -F chat_id="${GID}" -F document=@/build.log https://api.telegram.org/bot"$BID"/sendDocument
+}
+
+function sendZip()
+{
+ cd /ak3 || exit
+ ZIP=$(echo *.zip)
+ curl -F chat_id="${GID}" -F document="@$ZIP"  https://api.telegram.org/bot"${BID}"/sendDocument
+}
+
+function zipper()
+{
+ cp ${DTBI} /ak3
+ cd /ak3 || exit 
+ make -j16
+ mv Thy-Kernel.zip Thy-K-"${TANGGAL}".zip
+}
+function success() 
+{
+ sendInfo "<b>Commit:</b><code>$(git --no-pager log --pretty=format:'"%h - %s (%an)"' -1)</code>" \
+          "<b>Compile Time:</b><code>$(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s)</code>" \
+          "<b>Toolchain:</b><code>${TOOL_VERSION}</code>" \
+          "<b>  proJTHy Success  </b>"
+ sendLog
+}
+
+function failed() 
+{
+ sendInfo "<b>Commit:</b><code>$(git --no-pager log --pretty=format:'"%h - %s (%an)"' -1)</code>" \
+          "<b>   ProJThy Failed  </b>" \
+          "Total Time Elapsed:$(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+ sendLog
+ exit 1;
+ }
+
+
+function compile() 
+{
+
+  cd /wahoo || exit
+  START=$(date +"%s")
+  make ARCH=arm64 mido_defconfig O=out 
+  PATH="/p-clang/bin/:${PATH}" \
+  make O=out -j16 &> /build.log \
+  CC=clang \
+  CLANG_TRIPLE=aarch64-linux-gnu- \
+  CROSS_COMPILE=aarch64-linux-gnu- \
+  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+			
+  if ! [ -a $DTBI ]; then																		
+   END=$(date +"%s")
+   DIFF=$(($END - $START))
+   failed
+  fi
+
+  END=$(date +"%s")
+  DIFF=$(($END - $START))
+  success
+  zipper
+  sendZip
+}
+
+compile
